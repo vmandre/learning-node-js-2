9ef80944de81edca3ac1bb13c571b0f32b52b2d0
Vanderlei <vanderlei.andre.ext@tivit.com>
2019-03-05 08:09:55 -0300

    Creating a get message service

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+var messages = [
+    {name: "Tim", message: "Hi"},
+    {name: "Jane", message: "Hello"}
+]
+
+app.get("/messages", (req, res) => {
+    res.send(messages)
+})
+
 var server = app.listen(3000, () => {
     console.log('server is running on port', server.address().port);
     
